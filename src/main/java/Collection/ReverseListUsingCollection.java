cd69cba4578177c4c68b0d8acd66120c13b59614
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2025-12-19 20:36:33 +0530

    reverse list using collection

---
+++ src/main/java/Collection/ReverseListUsingCollection.java
@@ -0,0 +1,17 @@
+package Collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseListUsingCollection {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6));
+
+        Collections.reverse(list);
+        System.out.println(list);
+    }
+}
